4bda6099c4e94e711782f68bb4d7231c9126360b
jadetulda1996 <jadetulda1996@gmail.com>
2018-09-30 22:28:59 +0800

    interpreter function in views.py of WebApp directory was updated.. Error states something like tab/indent

--- WebApp/views.py
+++ WebApp/views.py
@@ -1,22 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . import api
 
 def index(request):
     return render(request, 'WebApp/home.html')
 
 def interpreter(request):
-	message = request.GET.get('msg')
-	
-	# Tokenizer
-	tokens = api.cfpl_tokenize(message)
-
-	# Lexical analyzer
-	lex = api.cfpl_lexer(tokens)
-
-	return HttpResponse(lex)
+	#result = "this is the result"
+	#context = {'result':result}
+    #return render(request, 'WebApp/home.html', context)
+    return render(request, 'WebApp/interpreter.html')
 
 def result(request):
     return render(request, 'WebApp/result.html')
+
 
 # Create your views here.
