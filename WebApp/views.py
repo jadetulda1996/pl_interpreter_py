344bf2f3b7ae4805f65db16dce2b23321bea5bc4
jadetulda1996 <jadetulda1996@gmail.com>
2018-09-30 22:56:06 +0800

    Revert "interpreter function in views.py of WebApp directory was updated.."
    This reverts commit 4bda6099c4e94e711782f68bb4d7231c9126360b.

--- WebApp/views.py
+++ WebApp/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import api
 
 def index(request):
     return render(request, 'WebApp/home.html')
 
 def interpreter(request):
-	#result = "this is the result"
-	#context = {'result':result}
-    #return render(request, 'WebApp/home.html', context)
-    return render(request, 'WebApp/interpreter.html')
+	message = request.GET.get('msg')
+	
+	# Tokenizer
+	tokens = api.cfpl_tokenize(message)
+
+	# Lexical analyzer
+	lex = api.cfpl_lexer(tokens)
+
+	return HttpResponse(lex)
 
 def result(request):
     return render(request, 'WebApp/result.html')
-
 
 # Create your views here.
--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,21 @@ $('#clicking').on('click', function(){
 });
 
 $('#btnGenerate').on('click', function(){
-    $('#result').html($('#exampleInputEmail1').val());
-    // alert("you clicked the button!");
+    //$('#result').html($('#exampleInputEmail1').val());
+    var code = $('#editor').val();    
+	$.ajax({
+		type: "GET",
+        url: "/interpreter/",
+        data: { msg: code },
+        //contentType: "application/json", 
+        //dataType: 'json',
+        async:false,        
+        success: function(data, status){
+        	console.log("success python call....");
+            $('#result').val(data);
+        },
+        error: function(xhr, desc, err){
+        	console.log("something went wrong...");
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+    });
 });
