211d1c72939a36e9262ad3cac4765965c6db1984
Dexter CABUNGCAL <cabungde@ph.ibm.com>
2018-09-30 17:13:26 +0800

    some more fixes

--- WebApp/views.py
+++ WebApp/views.py
@@ -5,10 +5,10 @@ def index(request):
     return render(request, 'WebApp/home.html')
 
 def interpreter(request):
-
-	result = "this is the result"
-	context = {'result':result}
-    return render(request, 'WebApp/home.html', context)
+	#result = "this is the result"
+	#context = {'result':result}
+    #return render(request, 'WebApp/home.html', context)
+    return render(request, 'WebApp/interpreter.html')
 
 def result(request):
     return render(request, 'WebApp/result.html')
