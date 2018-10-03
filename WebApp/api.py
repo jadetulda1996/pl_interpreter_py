c019c05454ea45487806c15eec913004b0a273ca
Dexter CABUNGCAL <cabungde@ph.ibm.com>
2018-10-03 19:34:23 +0800

    more logic

--- WebApp/api.py
+++ WebApp/api.py
@@ -1,17 +1,19 @@
 import re
 
-reserved = ['VAR', 'OUTPUT', 'INPUT', 'AS']
-ar_operators = ['(', ')', '*', '/', '%', '+', '-', '>', '<', '>=', '<=', '==', '<>']
-as_operators = ['=']
+reserved = ['VAR', 'OUTPUT', 'INPUT', 'AS', 'START', 'STOP']
+arithmetic_operators = ['(', ')', '*', '/', '%', '+', '-', '>', '<', '>=', '<=', '==', '<>']
+assignment_operators = ['=']
+logical_operators = ['AND', 'OR', 'NOT']
 datatype = ['INT', 'CHAR', 'BOOL', 'FLOAT']
 
 def cfpl_tokenize(code):
-	from nltk.tokenize import word_tokenize
-	tokens = word_tokenize(code)
-	#code = code.replace('\n', ' NEWLINE ')
-	#tokens = code.split(' ')
+	#from nltk.tokenize import word_tokenize
+	#tokens = word_tokenize(code)
+	code = code.replace('\n', ' NEWLINE ')
+	tokens = code.split(' ')
 	print(tokens)
-	return tokens
+	filtered = filter(None, tokens) #remove all empty tokens
+	return filtered
 
 def cfpl_lexer(tokens):
 	lextoken = list()
@@ -29,10 +31,10 @@ def checkTokenType(token):
 		tokentype = "RESERVED"
 	elif token in datatype:
 		tokentype = "DATATYPE"
-	elif token in as_operators:
-		tokentype = "AS_OPERATOR"
-	elif token in ar_operators:
-		tokentype = "AR_OPERATOR"
+	elif token in assignment_operators:
+		tokentype = "ASSIGNMENT_OPS"
+	elif token in arithmetic_operators:
+		tokentype = "ARITHMETIC_OPS"
 	elif isIdentifier(token):
 		tokentype = "INDENTIFIER"
 	elif isDigit(token):
