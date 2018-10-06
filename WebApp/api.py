104e3d1ed14f41d096f20f4f20dde34d67baca75
Dexter CABUNGCAL <cabungde@ph.ibm.com>
2018-10-06 13:05:13 +0800

    added verify structure and statement

--- WebApp/api.py
+++ WebApp/api.py
@@ -1,42 +1,70 @@
 import re
 
-keyword = ['VAR', 'OUTPUT:', 'INPUT:', 'AS', 'START', 'STOP']
+output = "all most there...."
+dictionary = {}
+keyword = ['VAR', 'AS', 'START', 'STOP']
 arithmetic_operators = ['(', ')', '*', '/', '%', '+', '-', '>', '<', '>=', '<=', '==', '<>']
 assignment_operators = ['=']
 logical_operators = ['AND', 'OR', 'NOT']
 datatype = ['INT', 'CHAR', 'BOOL', 'FLOAT']
 
-def cfpl_tokenize(code):	
+def cfpl_tokenize(code):
+	global output	
 	statements = code.split('\n')
+	print(statements)
 	tokens = list()
 	for statement in statements:
 		if(statement):			
 			content = statement.strip()
-			tokens.append(checkStatementType(content) + ':' + content)
+			# tag each statement
+			tokens.append(getStatementType(content) + ':' + content)
 	print(tokens)
-	return "on testing"
-
-def cfpl_lexer(tokens):
-	lextoken = list()
-	for token in tokens:
-		lextoken.append(checkTokenType(token) + ":" + token)
-	return lextoken 
+	return tokens
 
 def cfpl_parse(statements):
-	return "check console for now..."
+	# lets scan
+	if(isValidStructure(statements)):
+		print(statements)
+		# TODO traverse the statement and work on OUTPUT statement
+	return output
 
-def checkStatementType(statement):
+def getStatementType(statement):
 	if isComment(statement):
 		return 'COMMENT'
 	elif isKeyword(statement):
 		return 'KEYWORD'
 	elif isVarDeclaration(statement):
 		return 'VARDEC'
+	elif isOutput(statement):
+		return 'OUTPUT'
 	else:
 		return 'INVALID'
 
-def isComment(statement):
-	return True if statement[0][0] == '*' else False
+def isValidStructure(statements):
+	global output
+	isValid = True
+	hasStarted = False
+	hasStop = False
+	linenumber = 1
+	for statement in statements:
+		print(hasStarted)
+		if(re.match('^INVALID', statement)):	
+			isValid = False		
+			output = "Syntax error in line " + repr(linenumber)
+			break
+		elif(re.match('^VARDEC', statement) and hasStarted == True):
+			isValid = False
+			output = "Invalid variable declaration in line " + repr(linenumber)
+			break
+		elif(re.match('^KEYWORD:START$', statement)):
+			hasStarted = True
+			print("went here")
+		else:
+			output = ""
+		linenumber += 1
+	#print(output)
+	return isValid
+
 
 def checkTokenType(token):
 	tokentype = ""
@@ -57,8 +85,12 @@ def checkTokenType(token):
 
 	return tokentype
 
+def isComment(statement):
+	return True if statement[0][0] == '*' else False
+
 def isKeyword(token):
-	return token in keyword
+	#return token in keyword
+	return re.match('^(VAR|AS|START|STOP)$', token)
 
 def isDatatype(token):
 	return token in datatype
@@ -69,19 +101,17 @@ def isAssignmentOperator(token):
 def isArithmeticOperator(token):
 	return token in arithmetic_operators
 
-def isIdentifier(token):
-	return re.match('[_a-zA-Z][_a-zA-Z0-9]{0,30}', token)
+#def isIdentifier(token):
+#	return re.match('[_a-zA-Z][_a-zA-Z0-9]{0,30}', token)
 
 def isDigit(token):
 	return re.match('\d+', token)
 
 def isVarDeclaration(statement):
-	# attempt to use regex to validate variable declaration syntax
+	# validate variable declaration syntax using regex
 	return re.match('^VAR\s[_a-zA-Z]+[0-9]*(=[_a-zA-Z0-9])?(,[_a-zA-Z0-9](=[_a-zA-Z0-9])?)*\sAS\s(INT|CHAR|BOOL|FLOAT)$', statement)
 
-def isVarDeclaration_xxx(statement):
-	tokens = statement.split(' ')
-	if tokens[0] == 'VAR':
-		if tokens[-1] in datatype:
-			if isIdentifier(tokens[1]):
-				return True
+def isOutput(statement):
+	# validate OUTPUT statement syntax using regex
+	return re.match('^OUTPUT:\s[_a-zA-Z0-9]', statement)
+	
